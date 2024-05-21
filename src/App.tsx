d05206b27bff93383515001ac18700bee6b8af12
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-05-21 21:43:02 +0600

    Level 3 is done

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,37 @@
 import './App.css'
 import Card from "./components/Cards/Card.tsx";
 import CardDeck from "./lib/CardDeck.ts";
-import {useState} from "react";
+import {useState } from "react";
+
+interface Cards {
+    rank: string;
+    suit:string;
+}
 
 const App = () => {
+    const [fiveRandomCards,setFiveRandomCards] = useState<Cards[]>([]);
 
-    const [deck] = useState(new CardDeck());
-    const [randomCards] = useState(deck.getCards(5));
 
     const getCards = () => {
-        console.log(deck)
-        console.log(randomCards)
+        const deck = new CardDeck();
+        const randomCards = deck.getCards(5);
+
+        setFiveRandomCards(randomCards)
     }
 
     return (
         <>
-        <div className="playingCards faceImages">
-            <Card rank={'k'} suit={'diams'}></Card>
-            <Card rank={'k'} suit={'spades'}></Card>
-        </div>
-            <button onClick={getCards}>click</button>
+            {fiveRandomCards.length > 0 ? <div>
+                <div className="playingCards faceImages">
+                    {fiveRandomCards.map((card,index) => (
+                        <Card rank={card.rank} suit={card.suit} key={index}></Card>
+                        ))}
+                </div>
+                <button onClick={getCards}>click</button>
+            </div> : <div>
+                <button onClick={getCards}>click</button>
+            </div>}
+
         </>
     )
 };
